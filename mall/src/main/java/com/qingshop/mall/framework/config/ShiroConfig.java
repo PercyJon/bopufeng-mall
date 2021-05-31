8f98c612e035e40aa526b0de5280afec2723c3da
PercyJon <PercyJon.H@gmail.com>
2021-05-31 13:27:13 +0800

    no commit message

--- mall/src/main/java/com/qingshop/mall/framework/config/ShiroConfig.java
+++ mall/src/main/java/com/qingshop/mall/framework/config/ShiroConfig.java
@@ -6,12 +6,14 @@ import java.util.Map;
 import javax.servlet.Filter;
 
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.codec.Base64;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.session.mgt.SessionManager;
-import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.servlet.SimpleCookie;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.apache.shiro.web.session.mgt.ServletContainerSessionManager;
 import org.springframework.beans.factory.annotation.Value;
@@ -28,9 +30,51 @@ import com.qingshop.mall.framework.shiro.web.UserPermFilter;
  */
 @Configuration
 public class ShiroConfig {
-	
-	@Value("${global.globalSessionTimeout}")
-    private int globalSessionTimeout;
+
+	@Value("${shiro.session.globalSessionTimeout}")
+	private int globalSessionTimeout;
+
+	/**
+	 * 设置Cookie的域名
+	 */
+	@Value("${shiro.cookie.domain}")
+	private String domain;
+
+	/**
+	 * 设置cookie的有效访问路径
+	 */
+	@Value("${shiro.cookie.path}")
+	private String path;
+
+	/**
+	 * 设置HttpOnly属性
+	 */
+	@Value("${shiro.cookie.httpOnly}")
+	private boolean httpOnly;
+
+	/**
+	 * 设置Cookie的过期时间，秒为单位
+	 */
+	@Value("${shiro.cookie.maxAge}")
+	private int maxAge;
+
+	/**
+	 * 设置cipherKey密钥
+	 */
+	@Value("${shiro.cookie.cipherKey}")
+	private String cipherKey;
+
+	/**
+	 * 登录地址
+	 */
+	@Value("${shiro.user.loginUrl}")
+	private String loginUrl;
+
+	/**
+	 * 权限认证失败地址
+	 */
+	@Value("${shiro.user.unauthorizedUrl}")
+	private String unauthorizedUrl;
 
 	/**
 	 * 单机环境，session交给shiro管理
@@ -75,14 +119,13 @@ public class ShiroConfig {
 		hashedCredentialsMatcher.setHashIterations(1024);
 		return hashedCredentialsMatcher;
 	}
-	
 
 	@Bean("securityManager")
 	public SecurityManager securityManager(SessionManager sessionManager) {
 		DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
 		securityManager.setRealm(myShiroRealm());
 		securityManager.setSessionManager(sessionManager);
-		securityManager.setRememberMeManager(null);
+		securityManager.setRememberMeManager(rememberMeManager());
 		return securityManager;
 	}
 
@@ -93,15 +136,15 @@ public class ShiroConfig {
 	public ShiroFilterFactoryBean shiroFilter(SecurityManager securityManager) {
 		ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
 		shiroFilterFactoryBean.setSecurityManager(securityManager);
-		shiroFilterFactoryBean.setLoginUrl("/login");
-		shiroFilterFactoryBean.setUnauthorizedUrl("/unauth");
+		shiroFilterFactoryBean.setLoginUrl(loginUrl);
+		shiroFilterFactoryBean.setUnauthorizedUrl(unauthorizedUrl);
 		// 自定义拦截器
 		Map<String, Filter> filtersMap = new LinkedHashMap<String, Filter>();
 		filtersMap.put("user", new UserFilter());
 		filtersMap.put("perms", new UserPermFilter());
 		shiroFilterFactoryBean.setFilters(filtersMap);
 		LinkedHashMap<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
-		
+
 		filterChainDefinitionMap.put("/app/**", "anon");
 		filterChainDefinitionMap.put("/common/**", "anon");
 		filterChainDefinitionMap.put("/images/**", "anon");
@@ -116,17 +159,34 @@ public class ShiroConfig {
 		shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
 		return shiroFilterFactoryBean;
 	}
-	
-	@Bean("lifecycleBeanPostProcessor")
-	public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
-		return new LifecycleBeanPostProcessor();
-	}
 
 	@Bean
 	public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
 		AuthorizationAttributeSourceAdvisor advisor = new AuthorizationAttributeSourceAdvisor();
 		advisor.setSecurityManager(securityManager);
 		return advisor;
+	}
+
+	/**
+	 * cookie 属性设置
+	 */
+	public SimpleCookie rememberMeCookie() {
+		SimpleCookie cookie = new SimpleCookie("rememberMe");
+		cookie.setDomain(domain);
+		cookie.setPath(path);
+		cookie.setHttpOnly(httpOnly);
+		cookie.setMaxAge(maxAge * 24 * 60 * 60);
+		return cookie;
+	}
+
+	/**
+	 * 记住我
+	 */
+	public CookieRememberMeManager rememberMeManager() {
+		CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
+		cookieRememberMeManager.setCookie(rememberMeCookie());
+		cookieRememberMeManager.setCipherKey(Base64.decode(cipherKey));
+		return cookieRememberMeManager;
 	}
 
 }
