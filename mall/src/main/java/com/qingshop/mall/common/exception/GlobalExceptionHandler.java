5e9ddc14a0464ee95fa1f3dabc7eb908e06a3783
PercyJon <PercyJon.H@gmail.com>
2021-06-25 21:42:22 +0800

    no commit message

--- mall/src/main/java/com/qingshop/mall/common/exception/GlobalExceptionHandler.java
+++ mall/src/main/java/com/qingshop/mall/common/exception/GlobalExceptionHandler.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.qingshop.mall.common.bean.Rest;
-import com.qingshop.mall.common.exception.BusinessException;
 import com.qingshop.mall.common.utils.ServletUtils;
 
 /**
