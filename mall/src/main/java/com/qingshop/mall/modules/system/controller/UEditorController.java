fd8502d0396ec2d8e9a7c54bb8384a3425d6a938
PercyJon <PercyJon.H@gmail.com>
2021-07-03 18:47:09 +0800

    no commit message

--- mall/src/main/java/com/qingshop/mall/modules/system/controller/UEditorController.java
+++ mall/src/main/java/com/qingshop/mall/modules/system/controller/UEditorController.java
@@ -1,12 +1,10 @@
 package com.qingshop.mall.modules.system.controller;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,19 +14,16 @@ import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileItemFactory;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.http.entity.ContentType;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.qingshop.mall.common.utils.StringUtils;
+import com.qingshop.mall.common.utils.UUIDUtil;
 import com.qingshop.mall.modules.common.BaseController;
 import com.qingshop.mall.modules.oss.OssFactory;
 import com.qingshop.mall.modules.system.vo.SysUploadFile;
@@ -97,60 +92,29 @@ public class UEditorController extends BaseController {
 		String[] imageArray = maps.get("source[]");
 		List<Map<String, Object>> replaceList = new ArrayList<Map<String, Object>>();
 		for (int i = 0; i < imageArray.length; i++) {
-			String imageStr = imageArray[i];
-			String fileType = imageStr.substring(imageStr.lastIndexOf(".") + 1);
-			MultipartFile upfile = createFileItem(imageStr, fileType);
-			SysUploadFile fileResult = Objects.requireNonNull(OssFactory.init()).uploadFile(upfile, true);
-			Map<String, Object> map = new HashMap<String, Object>();
-			map.put("size", upfile.getSize());
-			map.put("source", imageStr);
-			map.put("state", "SUCCESS");
-			map.put("title", upfile.getOriginalFilename());
-			map.put("url", fileResult.getFileUrl());
-			replaceList.add(map);
+			String source = imageArray[i];
+			if (StringUtils.isNotBlank(source)) {
+				URL url = new URL(source);
+				URLConnection con = url.openConnection();
+				try (InputStream is = con.getInputStream();) {
+					// 取出后缀名
+					String extensionName = source.substring(source.lastIndexOf("."), source.lastIndexOf(".") + 4);
+					String suffix = UUIDUtil.generateShortUuid() + extensionName;
+					SysUploadFile fileResult = Objects.requireNonNull(OssFactory.init()).upload(is, suffix, true);
+					Map<String, Object> map = new HashMap<String, Object>();
+					map.put("source", source);
+					map.put("state", "SUCCESS");
+					map.put("url", fileResult.getFileUrl());
+					replaceList.add(map);
+				} catch (Exception ex) {
+					logger.error(ex.getMessage(), ex);
+				}
+			}
+
 		}
 		json.put("state", "SUCCESS");
 		json.put("list", replaceList);
 		return json;
-	}
-
-	/**
-	 * url转变为 MultipartFile对象
-	 * 
-	 * @param url
-	 * @param fileName
-	 * @return
-	 * @throws Exception
-	 */
-	private static MultipartFile createFileItem(String url, String fileName) throws Exception {
-		FileItem item = null;
-		try {
-			HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
-			conn.setReadTimeout(30000);
-			conn.setConnectTimeout(30000);
-			// 设置应用程序要从网络链接读取数据
-			conn.setDoInput(true);
-			conn.setRequestMethod("GET");
-			if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
-				InputStream is = conn.getInputStream();
-				FileItemFactory factory = new DiskFileItemFactory(16, null);
-				String textFieldName = "uploadfile";
-				item = factory.createItem(textFieldName, ContentType.APPLICATION_OCTET_STREAM.toString(), false,
-						fileName);
-				OutputStream os = item.getOutputStream();
-				int bytesRead = 0;
-				byte[] buffer = new byte[8192];
-				while ((bytesRead = is.read(buffer, 0, 8192)) != -1) {
-					os.write(buffer, 0, bytesRead);
-				}
-				os.close();
-				is.close();
-			}
-		} catch (IOException e) {
-			throw new RuntimeException("文件下载失败", e);
-		}
-
-		return new CommonsMultipartFile(item);
 	}
 
 }
